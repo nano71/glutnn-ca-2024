9d76fb98f808e163f3a7a659228b2f0dd9c3775b
nano71 <master@nano71.com>
2024-10-30 14:45:33 +0800

    添加元素出现动画

--- js/image.js
+++ js/image.js
@@ -1,10 +1,17 @@
 function initializeImage() {
-    document.querySelectorAll('img').forEach(img => {
-        img.onload = function () {
-            console.log(img);
-            img.classList.add("show")
+    document.querySelectorAll("img").forEach(img => {
+        function handleImageLoad() {
+            img.classList.add("show");
         }
-    })
+
+        // 添加 load 事件监听器
+        img.addEventListener("load", handleImageLoad);
+
+        // 如果图片已加载完成，则立即执行
+        if (img.complete) {
+            handleImageLoad();
+        }
+    });
 }
 
-initializeImage()
+initializeImage();
